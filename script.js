13c67f58dcc53f8b1aa9413b73a3b220e8b73cca
jutin0852 <jutindikonu8@gmail.com>
2022-10-20 22:25:38 +0100

    added image modal

---
+++ script.js
@@ -0,0 +1,55 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    
+   
+  }
+// **********cart pop
+var cartshop=document.getElementById('cart');
+  function cart(){
+   if(cartshop.className==""){
+    cartshop.style.display="block"
+    cartshop.classList.add("cartt");
+   }
+   else{
+    cartshop.style.display="none";
+    cartshop.classList.remove("cartt");
+
+   }
+    
+
+
+    
+  }
