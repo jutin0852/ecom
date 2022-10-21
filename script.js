bfe230d53a8730a6fb54b7f0e254b9de824c793f
jutin0852 <jutindikonu8@gmail.com>
2022-10-22 00:08:47 +0100

    completed cart funct

--- script.js
+++ script.js
@@ -41,18 +41,39 @@ var cartshop=document.getElementById('cart');
 var minus=document.getElementById('minus');
 var zero=document.getElementById('zero');
 var cartnum=document.getElementById("cartnum");
-  function cart(){
-   if(cartshop.className==""){
-    cartshop.style.display="block"
-    cartshop.classList.add("cartt");
-   }
-   else{
-    cartshop.style.display="none";
-    cartshop.classList.remove("cartt");
+var current=document.getElementsByClassName('active')
+var smallImg=document.getElementsByClassName('smallimg');
+var cartItem=document.getElementById('cartitem');
+var emptyCart=document.getElementById('emcart');
+var price =document.getElementById('price');
+var incart=document.getElementById("incart")
 
-   }   
+
+
+  function cart(){
+      if(cartshop.className==""){
+            cartshop.style.display="block";
+            cartshop.classList.add("cartt");
+        if(!cartnum.textContent==""){
+          cartItem.style.display="block";
+        
+          price.innerHTML="$125.00 x "+cartnum.textContent+" <strong style='color:black;'>$"+125*Number(cartnum.textContent)+"</strong>"
+          emptyCart.style.display="none";
+        }
+        else{
+          cartItem.style.display="none";
+          emptyCart.style.display="block";
+
+        }
+      }
+      else{
+        cartshop.style.display="none";
+        cartshop.classList.remove("cartt");
+      }   
   }
 function add(){
+  cartshop.style.display="none";
+  cartshop.classList.remove("cartt");
   if(event.target.id=="minus"){
     if(zero.textContent>0){
       zero.textContent--;}}
@@ -62,9 +83,30 @@ function add(){
 
 }
 function btn(){
+  cartshop.style.display="none";
+  cartshop.classList.remove("cartt");
   
   if(zero.textContent>0){
     cartnum.style.display="block";
     cartnum.textContent=Number(cartnum.textContent)+Number( zero.textContent)
   }
 }
+//..............
+for(var x=0; x<smallImg.length; x++){
+  smallImg[x].addEventListener("click",function(e){
+    if(current.length>0){
+      current[0].className=current[0].className.replace(" active","");
+    }
+    e.target.className+=" active";
+
+  })
+}
+incart.addEventListener('click',function(e){
+  console.log(e.target)
+  if(e.target.id=="deleteCart"){
+   cartItem.style.display="none";
+   emptyCart.style.display="block";
+   cartnum.textContent="";
+   cartnum.style.display="none"
+  }
+})
